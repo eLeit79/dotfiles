09b6e268df4a39790a47993c6ff7a9c7073c64ee
Martin Enderleit <44668487+eLeit79@users.noreply.github.com>
2026-08-18 00:53:08 +0200

    more updates

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,9 +1,9 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 source ~/.config/fish/abbreviations/chezmoi.fish
 
-set -gx ANDROID_HOME $HOME/Android/Sdk
-fish_add_path $ANDROID_HOME/emulator
-fish_add_path $ANDROID_HOME/platform-tools
+#set -gx ANDROID_HOME $HOME/Android/Sdk
+#fish_add_path $ANDROID_HOME/emulator
+#fish_add_path $ANDROID_HOME/platform-tools
 
 # overwrite greeting
 function fish_greeting
@@ -12,5 +12,5 @@ end
 
 starship init fish | source
 
-nvm -s install latest
-nvm -s use latest
+#nvm -s install latest
+#nvm -s use latest
