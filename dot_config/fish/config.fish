9aa6e1576d9a801a134ad03532006879e7e1713e
Martin Enderleit <44668487+eLeit79@users.noreply.github.com>
2026-09-01 04:24:18 +0200

    better keybinds

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,8 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 source ~/.config/fish/abbreviations/chezmoi.fish
 source ~/.config/fish/aliases/nvim.fish
 
+set -gx _JAVA_AWT_WM_NONREPARENTING 1
+
 #set -gx ANDROID_HOME $HOME/Android/Sdk
 #fish_add_path $ANDROID_HOME/emulator
 #fish_add_path $ANDROID_HOME/platform-tools
